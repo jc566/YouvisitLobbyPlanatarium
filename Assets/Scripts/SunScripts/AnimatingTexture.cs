3f0cebae71b002fd7cc6d7198b31789bb7cf5995
Joseph Choi <jc566@njit.edu>
2017-06-08 18:32:39 -0400

    June 8 backup

---
+++ Assets/Scripts/SunScripts/AnimatingTexture.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatingTexture : MonoBehaviour {
+
+    public float speed = 0.1f;
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();        
+    }
+
+    void FixedUpdate()
+    {
+        float offset = speed * Time.time;
+
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
---
+++ Assets/Scripts/SupernovaScripts/Modded_fire_soft.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Modded_fire_soft : MonoBehaviour {
+    //Make reference to the particle system attached
+    public ParticleSystem PSone;
+
+    //Scale change vector values
+    public float scaleX = .1f;
+    public float scaleY = .1f;
+    public float scaleZ = .1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        alterPSone();
+	}
+
+    public void alterPSone()
+    {
+        //Make reference to the Emission of PSone.
+
+        //Make reference to the Scale of the PSone particle system
+        PSone.transform.localScale = new Vector3(scaleX += 0.003f, scaleY += 0.003f, scaleZ += 0.003f);
+    }
+}
